50dd374f7d6470ba5e9ab8cc07bfc592c6bdf52c
NtshVrm <42909334+NtshVrm@users.noreply.github.com>
2021-08-17 19:43:30 +0530

    docs: styling

--- src/App.js
+++ src/App.js
@@ -32,49 +32,53 @@ export default function App() {
       let lossPercent = (loss / buyingValue) * 100;
       var message = `Loss is ${lossPercent}% i.e Rs.${loss}.`;
       setmessage(message);
-      // if (lossPercent > 50) {
-      //     header.style.backgroundColor = 'red';
-      // }
     } else {
       profit = presentValue - buyingValue;
       let profitPercent = (profit / buyingValue) * 100;
       message = `Profit is ${profitPercent}% i.e Rs.${profit}.`;
       setmessage(message);
-      // if( profitPercent > 50) {
-      //     header.style.backgroundColor = 'green';
-      // }
     }
   }
 
   return (
     <div className="App">
-      <div className="header">Profit or Loss</div>
-      <h4>
-        Enter your stock purchase price, number of units of stock bought and
-        current price and the app will tell you how much profit or loss you have
-        incurred.
-      </h4>
+      <div className="header navigation container">PROFIT or LOSS</div>
 
-      <div className="purchase-price">
-        Enter Purchase Price:
-        <input onChange={purchaseHandler}></input>
-      </div>
+      <section className="section edu">
+        <div className="container container-center">
+          <div>
+            <img src="https://i.postimg.cc/3w7pdRGz/undraw-financial-data-es63.png"></img>
+          </div>
+          <h4 className="color-inverted">
+            Enter the stock <span className="alt-color">Purchase Price</span>,{" "}
+            <span className="alt-color">Number of Units</span> bought and it's{" "}
+            <span className="alt-color">Current Price</span> and the app will
+            calculate the amount of Profit/Loss you have incurred.
+          </h4>
+          <section className="ow">
+            <div className="purchase-price">
+              Enter Purchase Price:
+              <input onChange={purchaseHandler}></input>
+            </div>
 
-      <div className="units">
-        Enter Number of Units:
-        <input onChange={unitHandler}></input>
-      </div>
+            <div className="units">
+              Enter Number of Units:
+              <input onChange={unitHandler}></input>
+            </div>
 
-      <div className="current-price">
-        Enter Current Price:
-        <input onChange={currentHandler}></input>
-      </div>
+            <div className="current-price">
+              Enter Current Price:
+              <input onChange={currentHandler}></input>
+            </div>
+          </section>
 
-      <div className="check">
-        <button onClick={checker}>Check</button>
-      </div>
+          <div className="check">
+            <button onClick={checker}>Check</button>
+          </div>
 
-      <div className="result">{message}</div>
+          <div className="result">{message}</div>
+        </div>
+      </section>
     </div>
   );
 }
